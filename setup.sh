33caac6a78b8403e6d2afe65318dabd71d96544f
kpzhao <36727707+kpzhao@users.noreply.github.com>
2020-09-03 10:35:08 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y build-essential bc python curl git zip ftp libssl-dev
+sudo apt-get install -y build-essential bc python curl git zip libssl-dev
 cd $HOME
 git clone --depth=1 https://github.com/kpzhao/AnyKernel3
 git clone --depth=1 https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/clang-proton
